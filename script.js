646a2f68cc8b8de7ddc7bf11f7389e961e4d881d
julianty <alexanderjulianty@gmail.com>
2022-05-04 11:33:04 -0700

    Adds operate function to call basic operations

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply (a, b) {
 function divide (a, b) {
   return a / b
 }
+
+function operate (a, b, operation) {
+  switch (operation) {
+    case '+':
+      return add(a,b)
+      break;
+    case '-':
+      return subtract(a,b)
+      break;
+    case '*':
+      return multiply(a,b)
+      break;
+    case '/':
+      return divide(a,b)
+      break;
+    default:
+      console.log("Could not find operator");
+      return
+  };
+}
