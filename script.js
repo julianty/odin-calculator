70c7026b5987bd76926c21fd01346ff5ebfad88e
julianty <alexanderjulianty@gmail.com>
2022-05-05 11:17:01 -0700

    Creates appendText function to organize logic

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function operate (a, b, operation) {
     case '-':
       return subtract(a,b)
       break;
-    case '*':
+    case 'x':
       return multiply(a,b)
       break;
     case '/':
@@ -34,12 +34,62 @@ function operate (a, b, operation) {
   };
 }
 
-function displayController (event) {
-  console.log(event.target);
+function appendText (displayText, buttonText) {
+  // Checks for decimal points
+  if (buttonText == '.' && displayText.includes('.')) {
+    console.log("A '.' already exists");
+  } 
+  // Checks for leading 0
+  else if (displayText == "0" && buttonText != '.') {
+    displayText = buttonText;
+  } 
+  else {
+    displayText = displayText + buttonText;
+  }
+
+
+  return displayText
 }
+
+function displayController (event) {
+  const buttonClass = event.target.classList.value;
+  const buttonText = event.target.textContent;
+  let displayText = display.textContent;
+
+  if (buttonClass == "numpad") {
+    displayText = appendText(displayText, buttonText);
+  }
+
+  if (buttonClass == "operator") {
+    // Checks for '='
+    // There must be a number in the buffer
+    if (bufferAssigned == true && buttonText == "=") {
+      displayText = operate(buffer, parseFloat(displayText), operatorBuffer);
+    }
+    else {
+      buffer = parseFloat(displayText);
+      operatorBuffer = buttonText;
+      displayText = 0;
+      bufferAssigned = true;
+    }
+  }
+
+  if (buttonClass == "clear") {
+    displayText = "0";
+    buffer = 0;
+    bufferAssigned = false;
+    operatorBuffer = null;
+  }
+  display.textContent = displayText;
+}
+
+
 
 const display = document.querySelector("#display p");
 const buttonList = document.querySelectorAll('button');
+let buffer = 0;
+let bufferAssigned = false;
+let operatorBuffer = null;
 // console.log(buttonList);
 
 buttonList.forEach( button => {
